6863b32a163f7f3ac92b0763e007ed755a413c99
Henrique Degrecci <hdegrecci@gmail.com>
2023-07-25 20:58:06 -0300

    Move posts to server model

--- app/routes/posts._index.tsx
+++ app/routes/posts._index.tsx
@@ -1,20 +1,10 @@
 import { Link, useLoaderData } from "@remix-run/react";
 
+import { getPosts } from "~/models/post.server";
 import { json } from "@remix-run/node";
 
 export const loader = async () => {
-  return json({
-    posts: [
-      {
-        slug: "my-first-post",
-        title: "My First Post",
-      },
-      {
-        slug: "90s-mixtape",
-        title: "A Mixtape I Made Just For You",
-      },
-    ],
-  });
+  return json({ posts: await getPosts() });
 };
 
 export default function Posts() {
