e7929d4432ca0181cb05c110b2809dd8ae0276eb
Henrique Degrecci <hdegrecci@gmail.com>
2023-07-26 20:38:33 -0300

    Add new post form

--- app/routes/posts.admin.tsx
+++ app/routes/posts.admin.tsx
@@ -1,4 +1,4 @@
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
 import { getPosts } from "~/models/post.server";
 import { json } from "@remix-run/node";
@@ -24,7 +24,9 @@ export default function PostAdmin() {
             ))}
           </ul>
         </nav>
-        <main className="col-span-4 md:col-span-3">...</main>
+        <main className="col-span-4 md:col-span-3">
+          <Outlet />
+        </main>
       </div>
     </div>
   );
